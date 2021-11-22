3a9736627d4686bcbc19a245392abda17ed9f25c
Daniil Chuprov <alyaska56@gmail.com>
2021-11-22 17:34:24 +0200

    add project

---
+++ src/Todo/todoItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function TodoItem({todo, index}) {
+	return (
+		<li>
+			<span>
+				<input type="checkbox" />
+				<strong>{index + 1}</strong>
+					{todo.title}
+			</span>
+			<button>&times;</button>
+		
+		</li>
+	 )
+}
+
+TodoItem.propTypes = {
+	todo: PropTypes.object.isRequired,
+	index: PropTypes.number
+}
+
+export default TodoItem;
